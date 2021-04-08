1668cfc4334c3adbd0a5a2407252bf531b148852
Emewjin <76927618+Emewjin@users.noreply.github.com>
2021-04-08 16:57:18 +0900

    my first templates

--- src/controllers/videosController.js
+++ src/controllers/videosController.js
@@ -1,4 +1,37 @@
-export const trending = (req, res) => res.render("home", {pageTitle:"HOME"});
+const fakeUser = {
+    username: "emewjin",
+    loggedIn: true,
+};
+
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title: 1,
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id:1,
+        },
+        {
+            title: 2,
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id:2,
+        },
+        {
+            title: 3,
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id:3,
+        },
+    ];
+    res.render("home", {pageTitle:"HOME", videos});
+}
 export const watch = (req, res) => res.render("watch", {pageTitle:"VIDEO"});
 export const editVideos = (req, res) => res.render("edit", {pageTitle:"VIDEO"});
 export const search = (req, res) => res.send("search videos");
